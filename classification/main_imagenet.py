f9fbad7b1ff109d4656bbba959f7a3018b9461aa
Qiu Yuqing <qyuqing@umich.edu>
2022-02-20 20:34:52 -0500

    change

--- classification/main_imagenet.py
+++ classification/main_imagenet.py
@@ -209,7 +209,8 @@ def validate(args, val_loader, model, criterion, epoch):
 
     num_step = len(val_loader)
     with torch.no_grad():
-        for input, target in tqdm.tqdm(val_loader, total=num_step, ascii=True, mininterval=5):
+        for input, target in tqdm.tqdm(val_loader, total=num_step, ascii=True, mininterval=5):  
+            print(len(input()))
             input = input.to(device=device, non_blocking=True)
             target = target.to(device=device, non_blocking=True)
 
